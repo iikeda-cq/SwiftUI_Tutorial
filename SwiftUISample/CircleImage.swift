3323b1edde5cb16fd58045d9f041cc558335f38d
iikeda-cq <i.ikeda@caraquri.com>
2020-10-18 12:32:11 +0900

    Building Lists and Navigation

--- SwiftUISample/CircleImage.swift
+++ SwiftUISample/CircleImage.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
     var body: some View {
-        Image("turtleRock")
+        image
             .clipShape(Circle())
             .overlay(
-                Circle().stroke(Color.gray, lineWidth: 4))
+                Circle().stroke(Color.white, lineWidth: 4))
             .shadow(radius: 10)
     }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
