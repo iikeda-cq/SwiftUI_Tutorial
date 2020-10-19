c7a0f098760a4fd286a7ed99cc2883e72bbe6199
iikeda-cq <i.ikeda@caraquri.com>
2020-10-19 23:46:12 +0900

    HomeViewの追加

--- SwiftUISample/LandmarkList.swift
+++ SwiftUISample/LandmarkList.swift
@@ -11,7 +11,6 @@ struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
     
     var body: some View {
-        NavigationView {
             List {
                 Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites only")
@@ -26,13 +25,14 @@ struct LandmarkList: View {
                 }
             }
             .navigationBarTitle(Text("Landmarks"))
-        }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
-            .environmentObject(UserData())
+        NavigationView {
+            LandmarkList()
+                .environmentObject(UserData())
+        }
     }
 }
